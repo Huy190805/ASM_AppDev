32bf5c0d52427f0d9a15403163e32dc884db5ffc
Cao Hong Huy <conhuy123@gmail.com>
2025-03-23 15:42:03 +0700

    lan dau dung git

---
+++ app/src/main/java/com/example/asm2_ad_team1/Login.java
@@ -0,0 +1,59 @@
+package com.example.asm2_ad_team1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//import com.example.expensemng.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+
+    private TextView routeRes,routeFg;
+    private EditText username;
+    private EditText password;
+//    private FirebaseAuth auth;
+    private Firebase firebase;
+    private Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //route Register
+        routeRes = findViewById(R.id.routeRes);
+        routeRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+
+            }
+        });
+        username = findViewById(R.id.username);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/asm2_ad_team1/Register.java
@@ -0,0 +1,40 @@
+package com.example.asm2_ad_team1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//import com.example.expensemng.Models.User;
+//import com.example.expensemng.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+//import com.google.firebase.auth.AuthResult;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//import com.google.firebase.firestore.DocumentReference;
+//import com.google.firebase.firestore.FirebaseFirestore;
+
+public class Register extends AppCompatActivity {
+    private TextView routeLogin;
+//    private FirebaseAuth auth;
+    private Button btnres;
+    private TextInputEditText username, email, password, age;
+//    private User user;
+//    private FirebaseFirestore db;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+}
