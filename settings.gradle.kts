6d99eb36bd708963bfb675f4fd5543352ccb4c44
Huy190805 <146050034+Huy190805@users.noreply.github.com>
2025-04-02 18:26:40 +0700

    Fix merge

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,28 +24,5 @@ dependencyResolutionManagement {
 rootProject.name = "Asm2_AD_Team1"
 include(":app")
 
-pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
 
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "Asm2_AD_Team1"
-include(":app")
 
